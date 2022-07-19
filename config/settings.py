55a31f59ea1edeeb5843054fd3049c92f49a99cf
Thanh <thanhcoral@gmail.com>
2022-07-19 16:28:42 +0700

    update_env

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True
+DEBUG = config('DEBUG')
 ALLOWED_HOSTS = []
 
 LANGUAGE_CODE = 'en-us'
